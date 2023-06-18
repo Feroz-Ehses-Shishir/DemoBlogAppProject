163107bf4e0f926b0d38ab3fba9f4ad2891712fc
Shishir <Shishir@DESKTOP-RVLG5NL>
2023-06-17 17:36:19 -0700

    Add project files.

---
+++ DemoBlogAppProject/DB/Appdbcontext.cs
@@ -0,0 +1,16 @@
+﻿using DemoBlogAppProject.Models.DomainModel;
+using Microsoft.EntityFrameworkCore;
+
+namespace DemoBlogAppProject.DB
+{
+    public class Appdbcontext : DbContext
+    {
+        public Appdbcontext(DbContextOptions options) : base(options)
+        {
+
+        }
+
+        public DbSet<Post> Posts { get; set; }
+        public DbSet<Tag> Tags { get; set; }
+    }
+}
---
+++ DemoBlogAppProject/Models/DomainModel/Tag.cs
@@ -0,0 +1,11 @@
+﻿namespace DemoBlogAppProject.Models.DomainModel
+{
+    public class Tag
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public string DisplayName { get; set; }
+
+        public ICollection<Post> Posts { get; set; }
+    }
+}
